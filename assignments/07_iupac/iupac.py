98ba749108cf675998a8612eab8b4f10b02a9fc0
Hailey Buell <hbuell6@email.arizona.edu>
2021-10-15 13:10:17 -0700

    new file

---
+++ assignments/07_iupac/iupac.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+Author : hbuell6 <hbuell6@localhost>
+Date   : 2021-10-13
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('SEQ',
+                        metavar='str',
+                        nargs='+',
+                        help='input DNA sequence')
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output filename',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'),
+                        default=sys.stdout)
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    iupac = {
+        'A': 'A',
+        'C': 'C',
+        'G': 'G',
+        'T': 'T',
+        'U': 'U',
+        'R': '[AG]',
+        'Y': '[CT]',
+        'S': '[GC]',
+        'W': '[AT]',
+        'K': '[GT]',
+        'M': '[AC]',
+        'B': '[CGT]',
+        'D': '[AGT]',
+        'H': '[ACT]',
+        'V': '[ACG]',
+        'N': '[ACGT]'
+    }
+
+    for sequence in args.SEQ:
+        new_seq = ''
+        for letter in sequence:
+            if letter in iupac:
+                new_seq += iupac.get(letter)
+
+        # if args.outfile: 
+        #     print(sequence + new_seq, file=args.outfile)
+        #     print(f'Done, see output in "{args.outfile.name}"')
+        # else: 
+        #     print(sequence + new_seq)
+
+        print(sequence + new_seq, file=args.outfile)
+        print(f'Done, see output in "{args.outfile.name}"')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
