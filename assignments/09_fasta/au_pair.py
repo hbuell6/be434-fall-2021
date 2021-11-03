4f17a72204b9798601156f04bc617c9f9c5e67aa
Hailey Buell <hbuell6@email.arizona.edu>
2021-11-03 09:28:56 -0700

    homework

--- assignments/09_fasta/au_pair.py
+++ assignments/09_fasta/au_pair.py
@@ -7,6 +7,7 @@ Purpose: Split interleaved/paired reads
 
 import argparse
 from Bio import SeqIO
+import os
 
 
 # --------------------------------------------------
@@ -21,8 +22,7 @@ def get_args():
                         help='input FASTA files',
                         metavar='FILE',
                         nargs='+',
-                        type=argparse.FileType('rt'),
-                        default=None)
+                        type=argparse.FileType('rt'))
 
     parser.add_argument('-o',
                         '--outdir',
@@ -39,11 +39,23 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
+    out_dir = args.outdir
+
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+
     for fh in args.fasta:
+        root, ext = (os.path.splitext(os.path.basename(fh.name)))
+        forward = open(os.path.join(out_dir, root + '_1' + ext), 'wt')
+        reverse = open(os.path.join(out_dir, root + '_2' + ext), 'wt')
         reader = SeqIO.parse(fh, 'fasta')
-        for rec in reader:
-            print('ID :', rec.id)
-            print('Seq:', str(rec.seq))
+
+        for i, rec in enumerate(reader):
+            out_fh = forward if i % 2 == 0 else reverse
+            print(f'>{rec.id}', file=out_fh)
+            print(rec.seq, file=out_fh)
+
+    print(f'Done, see output in "{out_dir}"')
 
 
 # --------------------------------------------------
